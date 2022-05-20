d0b530b7faede7397bbbdfc45b95323a0b06a546
Ben DeWeerd <bendeweerd2001@gmail.com>
2022-05-20 15:58:04 -0400

    add julia notes

--- python_apis/spacex_api.py
+++ python_apis/spacex_api.py
@@ -18,13 +18,9 @@ class apiCall:
 		self.responseJSON = self.response.json()
 
 		self.responseDetails = self.responseJSON["details"]
-		if (self.responseJSON['success'] == 'true'):
-			self.success = True
-		else:
-			self.success = False
 		self.timestamp = self.responseJSON['date_unix']
 		self.time = datetime.fromtimestamp(self.timestamp)
-		self.name = self.responseJSON['name']
+		self.missionName = self.responseJSON['name']
 		self.missionPatchUrl = self.responseJSON['links']['patch']['small']
 		self.missionPicturesUrls = self.responseJSON['links']['flickr']['original']
 
@@ -32,6 +28,8 @@ class Interface:
 
 	def __init__(self, window):
 		
+		self.apiCall = apiCall()
+
 		#create main window and frame to hold widgets
 		self.window = window
 		self.mainFrame = tkinter.Frame(
@@ -44,18 +42,16 @@ class Interface:
 
 		#create fonts
 		self.titleFont = tkFont.Font(
-			family = "Arial", 
+			# family = "Arial",
+			family = "Magneto", 
 			size = 20,
 			weight = "bold"
 		)
 		self.detailsFont = tkFont.Font(
-			family = "Arial",
+			# family = "Arial",
+			family = "Magneto",
 			size = 12
 		)
-
-		self.apiCall = apiCall();
-
-		
 
 		self.titleFrame = tkinter.Frame(
 			master = self.mainFrame,
@@ -63,7 +59,7 @@ class Interface:
 		)
 		self.title = tkinter.Label(
 			master = self.titleFrame,
-			text = "Latest SpaceX launch: {}".format(self.apiCall.name),
+			text = "Latest SpaceX launch: {}".format(self.apiCall.missionName),
 			font = self.titleFont,
 			foreground = 'white',
 			background = 'black'
@@ -80,11 +76,12 @@ class Interface:
 		self.titleFrame.grid(column = 0, row = 0, padx = 20, pady = 20)
 
 		
-		self.patchUrlStream = urlopen(self.apiCall.responseJSON['links']['patch']['small'])
+		self.patchUrlStream = urlopen(self.apiCall.missionPatchUrl)
 		self.patchRawData = self.patchUrlStream.read()
 		self.patchUrlStream.close()
 		self.patchBytesImage = Image.open(BytesIO(self.patchRawData))
 
+		# patch image is square
 		self.patchImageWidth, self.patchImageHeight = self.patchBytesImage.size
 		self.patchImageCropPoints = self.get_crop_points(self.patchImageWidth, self.patchImageHeight)
 		self.patchBytesImage = self.patchBytesImage.crop(
@@ -104,35 +101,33 @@ class Interface:
 		)
 		self.patchLabel.grid(column = 1, row = 0, padx = 15, pady = 15)
 
-		self.collage1UrlStream = urlopen(self.apiCall.responseJSON['links']['flickr']['original'][0])
-		self.collage1RawData = self.collage1UrlStream.read()
-		self.collage1UrlStream.close()
-		self.collage1BytesImage = Image.open(BytesIO(self.collage1RawData))
+		# self.collage1UrlStream = urlopen(self.apiCall.responseJSON['links']['flickr']['original'][0])
+		# self.collage1RawData = self.collage1UrlStream.read()
+		# self.collage1UrlStream.close()
+		# self.collage1BytesImage = Image.open(BytesIO(self.collage1RawData))
 
-		self.collage1ImageWidth, self.collage1ImageHeight = self.collage1BytesImage.size
-		self.collage1ImageCropPoints = self.get_crop_points(self.collage1ImageWidth, self.collage1ImageHeight)
-		self.collage1BytesImage = self.collage1BytesImage.crop(
-			(self.collage1ImageCropPoints[0],
-			self.collage1ImageCropPoints[1],
-			self.collage1ImageCropPoints[2],
-			self.collage1ImageCropPoints[3])
-		)
-		self.collage1BytesImage = self.collage1BytesImage.resize((400, 400))
-		self.collage1Photo = ImageTk.PhotoImage(self.collage1BytesImage)
+		# self.collage1ImageWidth, self.collage1ImageHeight = self.collage1BytesImage.size
+		# self.collage1ImageCropPoints = self.get_crop_points(self.collage1ImageWidth, self.collage1ImageHeight)
+		# self.collage1BytesImage = self.collage1BytesImage.crop(
+		# 	(self.collage1ImageCropPoints[0],
+		# 	self.collage1ImageCropPoints[1],
+		# 	self.collage1ImageCropPoints[2],
+		# 	self.collage1ImageCropPoints[3])
+		# )
+		# self.collage1BytesImage = self.collage1BytesImage.resize((400, 400))
+		# self.collage1Photo = ImageTk.PhotoImage(self.collage1BytesImage)
 
-		self.collageFrame = tkinter.Frame(
-			master = self.mainFrame,
-			background = 'black'
-		)
-		self.imageLabel1 = tkinter.Label(
-			master = self.collageFrame,
-			image = self.collage1Photo,
-			background = 'black'
-		)
-		self.imageLabel1.pack()
-		self.collageFrame.grid(column = 0, row = 1, padx = 15, pady = 15)
-
-
+		# self.collageFrame = tkinter.Frame(
+		# 	master = self.mainFrame,
+		# 	background = 'black'
+		# )
+		# self.imageLabel1 = tkinter.Label(
+		# 	master = self.collageFrame,
+		# 	image = self.collage1Photo,
+		# 	background = 'black'
+		# )
+		# self.imageLabel1.pack()
+		# self.collageFrame.grid(column = 0, row = 1, padx = 15, pady = 15)
 
 		self.mainFrame.grid(column = 0, row = 0)
 
@@ -151,8 +146,6 @@ class Interface:
 
 		cropPoints = [cropLeft, cropTop, cropRight, cropBottom]
 		return cropPoints
-
-
 
 if __name__ == '__main__':
 	window = tkinter.Tk()
